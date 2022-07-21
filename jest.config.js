7a5c416501e8fd3883c447c79a758559c1673c5f
smikhalevski <smikhalevski@gmail.com>
2022-07-22 02:13:50 +0300

    Single package repo layout

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rootDir: process.cwd(),
-  preset: 'ts-jest',
-  modulePathIgnorePatterns: ['/lib/'],
-  moduleNameMapper: {
-    '^doubter$': __dirname + '/packages/doubter',
-    '^@doubter/(.*)$': __dirname + '/packages/$1/src/main',
-  },
-};
