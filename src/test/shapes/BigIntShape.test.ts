bc7e2861949826a2f0b0d8b21431626ee64f09c4
smikhalevski <smikhalevski@gmail.com>
2022-11-09 16:47:56 +0300

    Fixed BigIntShape tests

--- src/test/shapes/BigIntShape.test.ts
+++ src/test/shapes/BigIntShape.test.ts
@@ -3,32 +3,22 @@ import { CODE_TYPE, TYPE_BIGINT } from '../../main/constants';
 
 describe('BigIntShape', () => {
   test('allows a bigint', () => {
-    expect(new BigIntShape().validate(BigInt(111))).toBe(null);
+    const value = BigInt(111);
+
+    expect(new BigIntShape().parse(value)).toBe(value);
   });
 
   test('raises if value is not a bigint', () => {
-    expect(new BigIntShape().validate('aaa')).toEqual([
-      {
-        code: CODE_TYPE,
-        path: [],
-        input: 'aaa',
-        param: TYPE_BIGINT,
-        message: 'Must be a bigint',
-        meta: undefined,
-      },
-    ]);
+    expect(new BigIntShape().try('aaa')).toEqual({
+      ok: false,
+      issues: [{ code: CODE_TYPE, path: [], input: 'aaa', param: TYPE_BIGINT, message: 'Must be a bigint' }],
+    });
   });
 
   test('overrides message for type issue', () => {
-    expect(new BigIntShape({ message: 'xxx', meta: 'yyy' }).validate(111)).toEqual([
-      {
-        code: CODE_TYPE,
-        path: [],
-        input: 111,
-        param: TYPE_BIGINT,
-        message: 'xxx',
-        meta: 'yyy',
-      },
-    ]);
+    expect(new BigIntShape({ message: 'aaa', meta: 'bbb' }).try(111)).toEqual({
+      ok: false,
+      issues: [{ code: CODE_TYPE, path: [], input: 111, param: TYPE_BIGINT, message: 'aaa', meta: 'bbb' }],
+    });
   });
 });
