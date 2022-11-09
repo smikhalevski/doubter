6508bad4c57eff9a7637c94e45d6aa5a076bceb1
smikhalevski <smikhalevski@gmail.com>
2022-11-09 17:07:39 +0300

    Fixed NumberShape tests

--- src/test/shapes/NeverShape.test.ts
+++ src/test/shapes/NeverShape.test.ts
@@ -3,15 +3,9 @@ import { CODE_NEVER } from '../../main/constants';
 
 describe('NeverShape', () => {
   test('always raises', () => {
-    expect(new NeverShape().validate(111)).toEqual([
-      {
-        code: CODE_NEVER,
-        path: [],
-        input: 111,
-        param: undefined,
-        message: 'Must not be used',
-        meta: undefined,
-      },
-    ]);
+    expect(new NeverShape().try(111)).toEqual({
+      ok: false,
+      issues: [{ code: CODE_NEVER, path: [], input: 111, message: 'Must not be used' }],
+    });
   });
 });
