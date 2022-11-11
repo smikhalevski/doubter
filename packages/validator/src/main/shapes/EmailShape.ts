061c89cfbf583e640bcd2b230a6d15075cc1f027
smikhalevski <smikhalevski@gmail.com>
2022-11-11 20:35:52 +0300

    Added comments to EmailShape

--- packages/validator/src/main/shapes/EmailShape.ts
+++ packages/validator/src/main/shapes/EmailShape.ts
@@ -19,38 +19,80 @@ export class EmailShape extends ValidatorShape<IsEmailOptions> {
     return null;
   }
 
+  /**
+   * If `true`, the validator will also match `Display Name <email-address>`.
+   *
+   * @default false
+   */
   allowDisplayName(value = true) {
     return this._clone({ allow_display_name: value });
   }
 
+  /**
+   * If `true`, the validator will reject strings without the format `Display Name <email-address>`.
+   *
+   * @default false
+   */
   requireDisplayName(value = true) {
     return this._clone({ require_display_name: value });
   }
 
+  /**
+   * If `false`, the validator will not allow any non-English UTF8 character in email address' local part.
+   *
+   * @default true
+   */
   allowUtf8LocalPart(value = true) {
     return this._clone({ allow_utf8_local_part: value });
   }
 
+  /**
+   * If `false`, e-mail addresses without having TLD in their domain will also be matched.
+   *
+   * @default true
+   */
   requireTld(value = true) {
     return this._clone({ require_tld: value });
   }
 
+  /**
+   * If `true`, the validator will not check for the standard max length of an email.
+   *
+   * @default false
+   */
   ignoreMaxLength(value = true) {
     return this._clone({ ignore_max_length: value });
   }
 
+  /**
+   * If `true`, the validator will allow IP addresses in the host part.
+   *
+   * @default false
+   */
   allowIpDomain(value = true) {
     return this._clone({ allow_ip_domain: value });
   }
 
+  /**
+   * If `true`, some additional validation will be enabled, e.g. disallowing certain syntactically valid email addresses
+   * that are rejected by GMail.
+   *
+   * @default false
+   */
   domainSpecificValidation(value = true) {
     return this._clone({ domain_specific_validation: value });
   }
 
+  /**
+   * If the part of the email after the @ symbol matches one of the blacklisted hosts, the validation fails.
+   */
   hostBlacklist(hosts: string[]) {
     return this._clone({ host_blacklist: hosts });
   }
 
+  /**
+   * The validator will reject emails that include any of the characters in the string, in the name part.
+   */
   blacklistedChars(chars: string) {
     return this._clone({ blacklisted_chars: chars });
   }
