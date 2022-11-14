cf9a50563aeacbdab77aecfcc81f3190a9dde8fc
smikhalevski <smikhalevski@gmail.com>
2022-11-14 17:20:58 +0300

    Reverted to a single-package structure

--- src/main/shapes/index.ts
+++ src/main/shapes/index.ts
@@ -7,6 +7,16 @@ export { NeverShape } from './NeverShape';
 export { NumberShape } from './NumberShape';
 export { ObjectShape, KeysMode } from './ObjectShape';
 export { RecordShape } from './RecordShape';
-export { AnyShape, RedirectShape, ReplaceShape, Shape, TransformShape, ValueType } from './Shape';
+export {
+  AnyShape,
+  ExcludeShape,
+  OpaqueExclude,
+  OpaqueReplace,
+  RedirectShape,
+  ReplaceShape,
+  Shape,
+  TransformShape,
+  ValueType,
+} from './Shape';
 export { StringShape } from './StringShape';
 export { UnionShape } from './UnionShape';
