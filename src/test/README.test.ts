a2dc8706876bbf1e0a57a3f4a56822e10f1ea406
smikhalevski <smikhalevski@gmail.com>
2023-01-23 17:47:03 +0300

    Tests for cookbook examples

---
+++ src/test/README.test.ts
@@ -0,0 +1,23 @@
+import qs from 'qs';
+import * as d from '../main';
+
+describe('README', () => {
+  test('rename keys', () => {
+    const keyShape = d.enum(['foo', 'bar']).transform(value => value.toUpperCase() as 'FOO' | 'BAR');
+
+    const shape = d.record(keyShape, d.number());
+
+    expect(shape.parse({ foo: 1, bar: 2 })).toStrictEqual({ FOO: 1, BAR: 2 });
+  });
+
+  test('query strings', () => {
+    const queryShape = d.object({
+      name: d.string().optional().catch(),
+      age: d.int().coerce().gt(0).optional().catch(),
+    });
+
+    expect(queryShape.parse(qs.parse('name=Frodo&age=50'))).toEqual({ name: 'Frodo', age: 50 });
+
+    expect(queryShape.parse(qs.parse('age=-33'))).toStrictEqual({ age: undefined });
+  });
+});
