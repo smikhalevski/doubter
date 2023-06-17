c44c54c7b94752edbbd5e8f76dff7582a218c176
smikhalevski <smikhalevski@gmail.com>
2023-06-18 00:38:39 +0300

    Reverted jest.config.json

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
-  modulePathIgnorePatterns: ['/lib/'],
-  moduleNameMapper: {
-    '^doubter$': __dirname + '/src/main',
-    '^doubter/(.*)$': __dirname + '/src/main/$1',
-  },
-};
