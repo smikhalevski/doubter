c073a63e2f29177b6aa33e389152f5c2f70303ca
smikhalevski <smikhalevski@gmail.com>
2023-07-10 23:41:05 +0300

    Fixed tests

--- src/test/dsl/date.test-d.ts
+++ src/test/dsl/date.test-d.ts
@@ -2,6 +2,6 @@ import { expectType } from 'tsd';
 import * as d from '../../main';
 import { INPUT, OUTPUT } from '../../main/internal';
 
-expectType<Date>(d.date().toISOString()[INPUT]);
+expectType<Date>(d.date().iso()[INPUT]);
 
-expectType<string>(d.date().toISOString()[OUTPUT]);
+expectType<string>(d.date().iso()[OUTPUT]);
