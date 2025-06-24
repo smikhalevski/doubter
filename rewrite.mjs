007f20ccf925f41ddffbdd26e58db52961425691
smikhalevski <smikhalevski@gmail.com>
2025-06-24 12:32:03 +0300

    Release workflows

--- rewrite.mjs
+++ rewrite.mjs
@@ -4,6 +4,9 @@ process.chdir(process.argv[2]);
 
 for (const file of fs.readdirSync('.', { recursive: true })) {
   if (file.endsWith('.js')) {
-    fs.writeFileSync(file.replaceAll('.js', '.mjs'), fs.readFileSync(file, 'utf8').replaceAll(".js'", ".mjs'"));
+    fs.writeFileSync(
+      file.slice(0, -3) + '.mjs',
+      fs.readFileSync(file, 'utf8').replace(/(['"]\.[^'"]+)\.js/g, '$1.mjs')
+    );
   }
 }
